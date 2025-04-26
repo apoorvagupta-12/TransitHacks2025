2f44aed4e7c3dd988420ed6155821b215fe56cee
Peyton Nash <peytonnash@gmail.com>
2025-04-26 16:32:07 -0500

    NEW TABLES, DATA LOAD

---
+++ data_load.sql
@@ -0,0 +1,140 @@
+INSERT INTO .stations (station_id, station_name)
+VALUES
+    ("40900", "Howard"),
+    ("41190", "Jarvis"),
+    ("41300", "Morse"),
+    ("41400", "Loyola"),
+    ("41500", "Granville"),
+    ("41600", "Thorndale"),
+    ("41800", "Bryn Mawr"),
+    ("41900", "Berwyn"),
+    ("42000", "Argyle"),
+    ("42100", "Lawrence"),
+    ("42240", "Wilson"),
+    ("42300", "Sheridan"),
+    ("42500", "Addison"),
+    ("42690", "Belmont"),
+    ("42750", "Fullerton"),
+    ("42810", "North/Clybourn"),
+    ("42900", "Clark/Division"),
+    ("43000", "Chicago"),
+    ("43100", "Grand"),
+    ("40380", "Lake"),
+    ("40560", "Monroe"),
+    ("40590", "Jackson"),
+    ("41490", "Harrison"),
+    ("41500", "Roosevelt"),
+    ("41000", "Cermak-Chinatown"),
+    ("40190", "Sox-35th"),
+    ("40220", "47th"),
+    ("40510", "Garfield"),
+    ("40910", "63rd"),
+    ("40990", "69th"),
+    ("40240", "79th"),
+    ("41430", "87th"),
+    ("40450", "95th/Dan Ryan") ;
+    
+INSERT INTO .users (
+	first_name				
+	,last_name				
+	,date_of_birth			
+	,sex					
+	,email					
+	,phone_number			
+	,undergrad 				
+	,interests_sports		
+	,interests_politics		
+	,interests_marxism		
+	,interests_trains		
+	,interests_computers	
+	,interests_turtles		
+	,interests_life			
+	,interests_death		
+	#,image					
+	,instagram 				
+	,facebook
+	,twitter )
+VALUES
+	('PEYTON' 									#first_name
+	,'NASH'										#last_name
+	,10/10/1996									#date_of_birth
+	,'MALE' 									#sex
+	,'PRNASH@UCHICAGO.EDU'						#email			
+	,5038817750									#phone_number
+	,0											#undergrad 				
+	,1 											#interests_sports		
+	,1											#interests_politics		
+	,1											#interests_marxism		
+	,1											#interests_trains		
+	,1											#interests_computers	
+	,0											#interests_turtles		
+	,0											#interests_life			
+	,0											#interests_death		
+	#,#image					
+	,'https://www.instagram.com/nash_peyton/' 	#instagram 				
+	,NULL										#facebook
+	,NULL										#twitter 
+    ),
+	('BRADLEY' 									#first_name
+	,'STOLLER'									#last_name
+	,03/09/2001									#date_of_birth
+	,'MALE' 									#sex
+	,'BSTOLLER@UCHICAGO.EDU'					#email			
+	,6172330048									#phone_number
+	,0											#undergrad 				
+	,1 											#interests_sports		
+	,1											#interests_politics		
+	,1											#interests_marxism		
+	,1											#interests_trains		
+	,1											#interests_computers	
+	,0											#interests_turtles		
+	,0											#interests_life			
+	,1											#interests_death		
+	#,#image					
+	,'https://www.instagram.com/_bradleystoller/' 	#instagram 				
+	,NULL										#facebook
+	,NULL										#twitter 
+    ),
+	('APOORVA' 									#first_name
+	,'GUPTA'									#last_name
+	,08/12/2000									#date_of_birth
+	,'MALE' 									#sex
+	,'APOORVAGUPTA@UCHICAGO.EDU'							#email			
+	,7654303648									#phone_number
+	,0											#undergrad 				
+	,1 											#interests_sports		
+	,1											#interests_politics		
+	,1											#interests_marxism		
+	,1											#interests_trains		
+	,1											#interests_computers	
+	,0											#interests_turtles		
+	,0											#interests_life			
+	,1											#interests_death		
+	#,#image					
+	,'https://www.instagram.com/apoorvagupta_12/'#instagram 				
+	,NULL										#facebook
+	,NULL										#twitter 
+    ),
+	('KYLER' 									#first_name
+	,'ROSEN'									#last_name
+	,10/4/2002									#date_of_birth
+	,'MALE' 									#sex
+	,'KYLER@UCHICAGO.EDU'							#email			
+	,4152795534									#phone_number
+	,0											#undergrad 				
+	,1 											#interests_sports		
+	,1											#interests_politics		
+	,1											#interests_marxism		
+	,1											#interests_trains		
+	,1											#interests_computers	
+	,0											#interests_turtles		
+	,0											#interests_life			
+	,1											#interests_death		
+	#,#image					
+	,'https://www.instagram.com/kykatro/' 	#instagram 				
+	,NULL										#facebook
+	,NULL										#twitter 
+    )
+
+    
+        
